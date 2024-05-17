df1777ae66831906912cbf02c9e23ce2117a1850
antaresjeet <jeetxo@outlook.com>
2024-05-17 10:18:55 +0530

    responsive css updated

--- src/components/game-over/index.tsx
+++ src/components/game-over/index.tsx
@@ -5,24 +5,26 @@ import { GameOverComponentProps } from "@/declarations";
 export default function GameOver({ isWin, restartGame }: GameOverComponentProps): JSX.Element {
   return (
     <Fragment>
-      {isWin
-        ?
-        <div className="winner-card float-card">
-          <div className="win-wrapper float-card-body">
-            <div className="card-title">Victory is yours!<br />What&apos;s your next move, champion?</div>
-            <PlayButton clickHandler={restartGame} beforeHoverText={"play"} afterHoverText={"again!"} />
-          </div>
-        </div>
-        :
-        <div className="looser-card float-card">
-          <div className="loose-wrapper float-card-body">
-            <div className="card-title">
-              Haha, better luck next time, slowpoke! <br />
+      <div className="main-container">
+        {isWin
+          ?
+          <div className="winner-card float-card">
+            <div className="win-wrapper float-card-body">
+              <div className="card-title">Victory is yours!<br />What&apos;s your next move, champion?</div>
+              <PlayButton clickHandler={restartGame} beforeHoverText={"play"} afterHoverText={"again!"} />
             </div>
-            <PlayButton clickHandler={restartGame} beforeHoverText={"try"} afterHoverText={"again!"} />
           </div>
-        </div>
-      }
+          :
+          <div className="looser-card float-card">
+            <div className="loose-wrapper float-card-body">
+              <div className="card-title">
+                Haha, better luck next time, slowpoke! <br />
+              </div>
+              <PlayButton clickHandler={restartGame} beforeHoverText={"try"} afterHoverText={"again!"} />
+            </div>
+          </div>
+        }
+      </div>
     </Fragment>
   );
 };
